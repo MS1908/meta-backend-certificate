62375c28008dc7c25e5e981aeb84edc3dc746d85
Ngô Quang Minh 54 (AI) <minhnq54@fpt.com.vn>
2023-02-09 14:03:10 +0700

    Initial commit for menu item API endpoints

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your views here.
+
+
+class ListMenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer

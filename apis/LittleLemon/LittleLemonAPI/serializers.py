bad0c182f6bc81b385308efd8b3eba4ed122a2a9
ms1908 <mathstudent1902@gmail.com>
2023-02-23 20:48:37 +0700

    updates

--- apis/LittleLemon/LittleLemonAPI/serializers.py
+++ apis/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import Cart, MenuItem, OrderItem, Order
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'featured', 'category']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    menuitem = MenuItemSerializer()
+
+    class Meta:
+        model = Cart
+        fields = ['user', 'menuitem', 'quantity', 'unit_price', 'price']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = []
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = []
--- apis/LittleLemon/restaurant/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Booking, Menu
-
-
-admin.site.register(Menu)
